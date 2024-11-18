dfb1976fd6dba46de12ee173cf0933c4b36709a2
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2024-11-18 18:29:44 +0530

    Add files via upload

---
+++ Ass1.R
@@ -0,0 +1,29 @@
+a<-23.4
+b<-45
+c<-678
+print(a)
+print(b)
+print(c)
+rm(c)
+ls()
+d<-"I am learning R"
+print(d)
+firstname<-"Bhawneet"
+lastname<-"Singh"
+print(paste(firstname,lastname,sep=" "))
+bool<-TRUE
+
+a=1
+switch(
+  a,
+  1:"Hello",
+  2:"Hi",
+  3:"Bye"
+)
+
+print(exp(2))
+print(log(10))
+print(log10(10))
+print(log2(10))
+print(pi)
+print(sqrt(4))
